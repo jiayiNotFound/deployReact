cb2bdc46f0c92e364f3ea9710bed813578074b7f
Jiayi <jiayi.gmail.com>
2023-08-19 17:35:40 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -22,3 +22,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 
 export type AppStore = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+
